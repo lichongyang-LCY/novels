9d2c489e0aecde60ea476e73cf66a44851c74719
lenovo <lichongyang@163.com>
2017-08-28 09:30:27 +0800

    secend

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# novels
+#novel
+
+先npm install
+再node app即可自动运行
+如果80端口被占，请自行到app.js修改端口
+默认访问地址http://localhost
---
+++ public/mui/js/index.js
@@ -0,0 +1,7 @@
+/**
+ * Created by lenovo on 2017/8/25.
+ */
+$(document).ready(function() {
+    var search=$("#search").val();
+    console.log("search:---------"+search);
+});
