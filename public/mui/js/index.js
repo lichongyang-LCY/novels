1fc36e171c166edc0a23aff884516fffd7dd82e1
lenovo <lichongyang@163.com>
2017-08-29 18:31:06 +0800

    搜索

--- public/mui/js/index.js
+++ public/mui/js/index.js
@@ -4,9 +4,18 @@
 $(document).ready(function() {
     $("#searchButton").click(function(){
         var search =$("#search").val();
+        var novelsList=$("#novelsList").val();
+        var newList = novelsList.substring(-1,(novelsList-1))
         console.log("search:---------"+search);
-        // for (var i=0;i< novels.length;i++){
-        //
-        // }
+        console.log("novelsList:---------"+JSON.stringify(newList));
+        for (var i=0;i< novelsList.length;i++){
+            // console.log("name:------"+novelsList[i].name);
+            // console.log("novelsList aa:---------"+JSON.stringify(novelsList[i]));
+            if (novelsList[i].name==search){
+                console.log("success!");
+            }else {
+                console.log("fail");
+            }
+        }
     });
 });
